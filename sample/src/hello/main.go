5f6d8eee02fdb999b34345e7367ecc3e4c2f7c9e
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-12-27 20:48:36 +0100

    update file sample/src/hello/main.go

--- sample/src/hello/main.go
+++ sample/src/hello/main.go
@@ -28,10 +28,11 @@ type poemWithTitle struct {
 var c config
 
 func poemHandler(w http.ResponseWriter, r *http.Request) {
-	// curl localhost:8080/poem?name=wordsworth | jq
+	// Get the poem name (curl localhost:8080/poem?name=wordsworth).
 	r.ParseForm()
 	poemName := r.Form["name"][0]
 
+	// Check the poem is valid as defined by the config file.
 	found := false
 	for _, v := range c.ValidPoems {
 		if v == poemName {
@@ -39,12 +40,12 @@ func poemHandler(w http.ResponseWriter, r *http.Request) {
 			break
 		}
 	}
-
 	if !found {
 		http.Error(w, "File not found (invalid)", http.StatusNotFound)
 		return
 	}
 
+	// Load the poem from the disk.
 	p, err := poetry.LoadPoem(poemName)
 	if err != nil {
 		//log.Fatal(err)
@@ -58,6 +59,7 @@ func poemHandler(w http.ResponseWriter, r *http.Request) {
 	//pwt := poemWithTitle{poemName, p, strconv.Itoa(p.NumWords()), p.NumThe()}
 	pwt := poemWithTitle{poemName, p, p.NumWords(), p.NumThe()}
 
+	// Encode poem as JSON.
 	enc := json.NewEncoder(w)
 	err = enc.Encode(pwt)
 	if err != nil {
@@ -66,11 +68,13 @@ func poemHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// Open config file.
 	f, err := os.Open("config")
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Read in the configuration from the config file.
 	dec := json.NewDecoder(f)
 	err = dec.Decode(&c)
 	f.Close()
@@ -78,6 +82,7 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Setup and start web server.
 	http.HandleFunc(c.Route, poemHandler)
 	http.ListenAndServe(c.BindAddress, nil)
 }
